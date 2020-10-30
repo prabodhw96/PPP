69fcfbd748ca0132746aef1e05406a4bf14dade2
prabodhw96 <prabodhwankhede1996@gmail.com>
2020-10-30 15:58:20 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import numpy as numpy
+import pandas as pd
+import streamlit as st
+
+DATA_URL = (
+    "D:/Prabodh/PPP Converter/data.csv"
+)
+
+st.title("Salary Converter")
+st.markdown("How much money would you need in London to buy the same things you would buy in New York? Convert salary using purchasing power parity.")
+
+data = pd.read_csv(DATA_URL)
+
+source = st.selectbox("Source country", list(data.Country))
+salary = st.number_input("Salary in source country's currency")
+target = st.selectbox("Target country", list(data.Country))
+
+def convert_by_ppp(source_country, target_country, salary):
+    return salary / list(data[data["Country"]==source_country]["2019"])[0] * list(data[data["Country"]==target_country]["2019"])[0]
+
+val_ppp = convert_by_ppp(source, target, salary)
+val_ppp = round(val_ppp, 2)
+source_curr = data[data["Country"]==source]["Currency"].reset_index(drop=True)[0]
+target_curr = data[data["Country"]==target]["Currency"].reset_index(drop=True)[0]
+st.write("You will need", val_ppp, target_curr, "in", target, "to buy the same things you would in", source, "with", salary, source_curr)
+
+st.text("")
+st.write("Data source: https://data.worldbank.org/indicator/PA.NUS.PRVT.PP")
