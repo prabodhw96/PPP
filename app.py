9e36ac8b92a925ad16d185ab00c40ac643234cce
prabodhw96 <prabodhwankhede1996@gmail.com>
2021-01-11 12:42:22 +0530

    awesome streamlit

--- app.py
+++ app.py
@@ -1,28 +1,23 @@
-import numpy as numpy
-import pandas as pd
 import streamlit as st
 
-DATA_URL = (
-    "data.csv"
-)
+import awesome_streamlit as ast
+import converter
 
-st.title("Salary Converter")
-st.markdown("How much money would you need in London to buy the same things you would buy in New York? Convert salary using purchasing power parity.")
+ast.core.services.other.set_logging_format()
 
-data = pd.read_csv(DATA_URL)
+PAGES = {
+	"Salary Converter": converter,
+}
 
-source = st.selectbox("Source country", list(data.Country))
-salary = st.number_input("Salary in source country's currency")
-target = st.selectbox("Target country", list(data.Country))
+def main():
+	#st.markdown("<h1 style='text-align: center;'>NPI RL</h1>", unsafe_allow_html=True)
+	st.sidebar.title("Navigation")
+	selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 
-def convert_by_ppp(source_country, target_country, salary):
-    return salary / list(data[data["Country"]==source_country]["2019"])[0] * list(data[data["Country"]==target_country]["2019"])[0]
+	page = PAGES[selection]
 
-val_ppp = convert_by_ppp(source, target, salary)
-val_ppp = round(val_ppp, 2)
-source_curr = data[data["Country"]==source]["Currency"].reset_index(drop=True)[0]
-target_curr = data[data["Country"]==target]["Currency"].reset_index(drop=True)[0]
-st.write("You will need", val_ppp, target_curr, "in", target, "to buy the same things you would in", source, "with", salary, source_curr)
+	with st.spinner(f"Loading {selection}..."):
+		ast.shared.components.write_page(page)
 
-st.text("")
-st.write("Data source: https://data.worldbank.org/indicator/PA.NUS.PRVT.PP")
+if __name__ == "__main__":
+	main()
